946bfe8d7a4de9d8302f3fa6717cb96699f7dcab
ruslankharakhashyan <ruslankharakhashyan@outlook.com>
2019-04-09 14:19:55 +0700

    first commit

---
+++ src/js/layouts/Full/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+  Container
+} from 'reactstrap';
+
+export const FooterIntro = () => (
+  <div className="site-footer">
+    <Container>
+      Footer
+    </Container>
+  </div>
+);
+
+export default FooterIntro;
